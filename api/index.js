3b718f7eecabe8482722aa0b583cfb2e5fb5de80
uday-bhatiya <udaybhatiya31@gmail.com>
2025-03-19 13:08:01 +0530

    changes

--- api/index.js
+++ api/index.js
@@ -17,7 +17,7 @@ const app = express();
 const _dirname = path.resolve()
 
 app.use(cors({
-    origin: "http://localhost:5173", 
+    origin: "https://employee-management-system-lhu2.onrender.com/", 
     credentials: true,
 }));
 app.use(express.json());
